55d98c42ecbc03c5e0bf281ef62487c74eb1ec71
samerdata <i.samer14231@gmail.com>
2024-09-30 19:44:20 +0300

    Add files via upload
    this is my method

---
+++ methodsam.py
@@ -0,0 +1,20 @@
+# this class is just for your method that you create
+def say_hello_to(name):
+  """Greets the given name.
+
+  Args:
+      name: The name to greet.
+
+  Returns:
+      A greeting message.
+  """
+                               
+  return f"Hello {name}"
+
+# Example usage
+print(say_hello_to("Osama"))
+print(say_hello_to.__doc__)
+print(help(say_hello_to))
+
+#this code donot work in train.py1  (i add 1 just to let it note)
+
